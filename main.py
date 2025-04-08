e15bf7dad9d229df2563804225cb90f6dd6add5a
Kenji Campos <kenjicanpos829@gmail.com>
2025-04-08 23:47:59 +0800

    add update_playlist

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 #TODO(JohnPaul Rodriguez): create a function that adds songs to the playlist
 #TODO(Kenji Enishi Campos): create a function that updates a song in the playlist
+def update_playlist():
+    song_id = int(input("Enter the song ID to update: "))
+    song_to_update = None
+
+    for song in playlist:
+        if song["song_id"] == song_id:
+            song_to_update = song
+            break
+
+    if song_to_update:
+        print(
+            f"Are you sure you want to update: " 
+            f"{song_to_update['song_name']}?"
+            )
+
+        choice = input("Enter Y to update or N to cancel: ").upper()
+
+        if choice == "Y":
+            new_song_name = input("Enter the new song name: ").title()
+            new_artist_name = input("Enter the new artist name: ").title()
+            new_song_genre = input("Enter the new song genre: ").upper()
+            new_release_year = int(input("Enter the new release year: "))
+
+            song_to_update["song_name"] = new_song_name
+            song_to_update["artist_name"] = new_artist_name
+            song_to_update["song_genre"] = new_song_genre
+            song_to_update["release_year"] = new_release_year
+
+            print("Song updated successfully.")
+        else:
+            print("Update cancelled.")
+    else:
+        print("Song ID not found.")
+
 #TODO(Jedi Duncan Gonot): create a function that displays all songs in the playlist
 #TODO(Paul Benidict Reduta): create a function that searches for a specific son
 #TODO(Dion Manicio): create a function that deletes a song in the playlist
@@ -42,7 +76,9 @@ while True:
         case 1:
             pass
         case 2:
-            pass
+            print(" ")
+            update_playlist()
+            input("Press any key to continue...")
         case 3:
             pass
         case 4:
